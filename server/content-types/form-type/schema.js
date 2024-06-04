363ae596dda810bb46b1cb50a144f965a8c0bda2
Abhishek Naik <abhishek@tentwenty.me>
2024-06-04 12:07:18 +0530

    updated homepage

--- server/content-types/form-type/schema.js
+++ server/content-types/form-type/schema.js
@@ -57,6 +57,14 @@ module.exports = {
         target: "plugin::strapi-v4-form-builder.form-email-template",
         mappedBy: "formTypes",
       },
+      formOrder: {
+        pluginOptions: {
+          i18n: {
+            localized: false,
+          },
+        },
+        type: "integer",
+      },
     },
   },
 };
