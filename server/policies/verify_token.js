4cf921031c7617bd79ceb2ad715a4643d8235b0a
Abhishek Naik <abhishek@tentwenty.me>
2024-06-06 16:39:34 +0530

    policy fixed

--- server/policies/verify_token.js
+++ server/policies/verify_token.js
@@ -4,22 +4,27 @@ const { parseMultipartData, errors } = require("@strapi/utils");
 const { PolicyError } = errors;
 
 module.exports = async (ctx, next) => {
-  let requestBody = ctx.request.body;
-  if (ctx.is("multipart")) {
-    const { data } = parseMultipartData(ctx);
-    requestBody = data;
+  try {
+    let requestBody = ctx.request.body;
+    const contentType = ctx.request.headers["content-type"];
+    if (contentType.startsWith("multipart/form-data")) {
+      const { data } = parseMultipartData(ctx);
+      requestBody = JSON.parse(data.data);
+    }
+    const { formType, csrfToken } = requestBody;
+    const formTypeRecord = await strapi.entityService.findOne(
+      formTypeModel,
+      formType
+    );
+    if (!formTypeRecord) {
+      throw new PolicyError("Invalid Form Type");
+    }
+    const isValid = verifyToken(csrfToken);
+    if (!isValid) {
+      throw new PolicyError("Invalid CSRF Token");
+    }
+    return isValid;
+  } catch (error) {
+    throw new PolicyError("Invalid Request");
   }
-  const { formType, csrfToken } = requestBody;
-  const formTypeRecord = await strapi.entityService.findOne(
-    formTypeModel,
-    formType
-  );
-  if (!formTypeRecord) {
-    throw new PolicyError("Invalid Form Type");
-  }
-  const isValid = verifyToken(csrfToken);
-  if (!isValid) {
-    throw new PolicyError("Invalid CSRF Token");
-  }
-  return isValid;
 };
